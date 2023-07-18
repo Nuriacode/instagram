a89c03609ff444473e304353d32d8026952a894b
Nuria Castaño <codesalnuria@gmail.com>
2023-07-18 14:52:17 +0200

    add styles of dashboard user

--- src/components/profile/User/UserDashboard.js
+++ src/components/profile/User/UserDashboard.js
@@ -7,51 +7,53 @@ import getDataPictures from "../../../services/apiImages";
 const Dashboard = () => {
   const [dataImages, setDataImages] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
     getDataPictures().then((dataImages) => {
       setDataImages(dataImages);
     });
   }, []);
 
-    const dataPicturesOk = dataImages.map ((eachPicture) => {
-      return (
-        <li className="dashboard__dataImages__li" key={eachPicture.id}>
-          <img className="dashboard__dataImages__li--img"src={eachPicture.url} alt="imagen del feed del usuario"/>
-        </li>
-      )
-    })
+  const dataPicturesOk = dataImages.map((eachPicture) => {
+    return (
+      <li className="dashboard__dataImages__li" key={eachPicture.id}>
+        <img
+          className="dashboard__dataImages__li--imgFeed"
+          src={eachPicture.url}
+          alt="imagen del feed del usuario"
+        />
+      </li>
+    );
+  });
 
   return (
     <>
       <NavProfile />
       <div className="dashboard">
         <div className="dashboard__dataUser">
-         <div className="dashboard__dataUser__imgUser">
-          <img
-          className="dashboard__dataUser__imgUser--img"
-          src={avatar_user}
-          alt="foto perfil de avatar"
-        />
-        </div>
-        
-        <div className="dashboard__dataUser__data">
-            <div>
-                <h2>Neytiri_avatar</h2>
-
-            </div>
-          <div className="dashboard__dataUser__data--followers">
-            <h3>413 publicaciones</h3>
-            <h3>713 seguidores</h3>
-            <h3>713 Seguidores</h3>
-            <h3>1171 seguidos</h3>
+          <div className="dashboard__dataUser__imgUser">
+            <img
+              className="dashboard__dataUser__imgUser--img"
+              src={avatar_user}
+              alt="foto perfil de avatar"
+            />
           </div>
-          <h2 className="dashboard__dataUser__data--description">Neytiri</h2>
+
+          <div className="dashboard__dataUser__data">
+            <div className="dashboard__dataUser__data__unserNAme">
+              <h2 className="dashboard__dataUser__data__userName--name">
+                Neytiri_avatar
+              </h2>
+            </div>
+            <div className="dashboard__dataUser__data__followers">
+              <h3 className="dashboard__dataUser__data__followers--textFollowers">413 publicaciones</h3>
+              <h3 className="dashboard__dataUser__data__followers--textFollowers">713 seguidores</h3>
+              <h3 className="dashboard__dataUser__data__followers--textFollowers">1171 seguidos</h3>
+            </div>
+            <h2 className="dashboard__dataUser__data--description">Neytiri</h2>
+          </div>
         </div>
-        </div>
-       
-        <ul className="dashboard__dataImages">
-          {dataPicturesOk}
-        </ul>
+
+        <ul className="dashboard__dataImages">{dataPicturesOk}</ul>
       </div>
     </>
   );
